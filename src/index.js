9054cc9e88b098cb1f665f817c76ed815b2eb4a8
Jesus <jesus.fdz93@gmail.com>
2023-07-08 15:07:22 -0500

    products

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ app.get("/api", (req, res) => {
 
 app.use("/api/usuarios", require("./routes/users.routes"));
 app.use("/api/auth", require("./routes/auth.routes"));
+app.use("/api/productos", require("./routes/products.routes"));
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
